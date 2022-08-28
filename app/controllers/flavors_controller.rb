e0ea8910f6df96ec05c16c2afe2cc8cd3d07b81e
CASEY BEHRENDT <behrendtc@gmail.com>
2022-08-28 11:38:16 -0600

    adds beer_flavors, flavors, flights controllers

---
+++ app/controllers/flavors_controller.rb
@@ -0,0 +1,12 @@
+class FlavorsController < ApplicationController
+  before_action :current_user
+  before_action :redirect_user
+
+  def index
+    @flavors = Flavor.all
+  end
+
+  def show
+    @flavor = Flavor.find(params[:id])
+  end
+end
---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,4 @@
+class FlightsController < ApplicationController
+  before_action :current_user
+  before_action :redirect_user
+end
